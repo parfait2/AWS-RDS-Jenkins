0c698c8a5d70cf0658979a8941db0833d5db3ce6
parfait2 <parkyl0208@naver.com>
2024-02-27 15:50:05 +0900

    update

---
+++ src/main/java/edu/fisa/lab/Step08ProbonoRdsApplication.java
@@ -0,0 +1,13 @@
+package edu.fisa.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step08ProbonoRdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step08ProbonoRdsApplication.class, args);
+	}
+
+}
